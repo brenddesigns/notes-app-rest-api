c9b9f87f210fa25ca07b50ef002d2856ddb5b905
brenddesigns <brenddesigns@hotmail.com>
2022-03-22 19:05:06 +1100

    Added Note model + Enabling JPA Auditing

---
+++ src/main/java/com/brendzn/notesapprestapi/model/Note.java
@@ -0,0 +1,44 @@
+package com.brendzn.notesapprestapi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private Long id;
+
+    @NotBlank
+    @Getter @Setter
+    private String title;
+
+    @NotBlank
+    @Getter @Setter
+    private String content;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    @Getter @Setter
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    @Getter @Setter
+    private Date updatedAt;
+}

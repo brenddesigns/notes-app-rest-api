f9be87fa2e798ce94fa77e45806d3c661d97888c
brenddesigns <brenddesigns@hotmail.com>
2022-03-22 19:08:38 +1100

    Added custom ResourceNotFoundException class

---
+++ src/main/java/com/brendzn/notesapprestapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,26 @@
+package com.brendzn.notesapprestapi.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    @Getter @Setter
+    private String resourceName;
+
+    @Getter @Setter
+    private String fieldName;
+
+    @Getter @Setter
+    private Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}

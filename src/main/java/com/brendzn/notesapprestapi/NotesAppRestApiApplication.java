d9d086de8e6c3ec99ec66a81374715e4256d0bef
brenddesigns <brenddesigns@hotmail.com>
2022-03-22 18:58:53 +1100

    Initial commit

---
+++ src/main/java/com/brendzn/notesapprestapi/NotesAppRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.brendzn.notesapprestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppRestApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brendzn/notesapprestapi/NotesAppRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brendzn.notesapprestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
